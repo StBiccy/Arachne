7a3d17b04283ac1bfe85d6a44313ec9c99c32c60
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2024-07-04 13:27:17 +0100

    initial commit

---
+++ Arachne/src/Test.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace arachne {
+	void Print()
+	{
+		printf("Welcome to arachne");
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,8 @@
+namespace arachne {
+	_declspec(dllimport) void Print();
+}
+
+void main()
+{
+	arachne::Print();
+}
